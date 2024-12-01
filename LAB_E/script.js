14d72bb6da9ec50d44a687f376bca6edb646ee6b
Jagodziński Cezary <cza-jag@wp.pl>
2024-12-01 20:22:01 +0100

    labE

--- LAB_E/script.js
+++ LAB_E/script.js
@@ -1,26 +1,19 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/*!***********************!*\
-  !*** ./src/script.ts ***!
-  \***********************/
-
-
-var styles = {
-  "styl1.css": "src/style/styl1.css",
-  "styl2.css": "src/style/styl2.css",
-  "styl3.css": "src/style/styl3.css"
+"use strict";
+const styles = {
+    "styl1.css": "src/style/styl1.css",
+    "styl2.css": "src/style/styl2.css",
+    "styl3.css": "src/style/styl3.css",
 };
 function changeStyle(styleName) {
-  console.log("Changing style to: ".concat(styleName));
-  var styleElement = document.getElementById("dynamic-style");
-  if (styleElement) {
-    styleElement.href = styles[styleName];
-    console.log("Style changed to: ".concat(styleElement.href));
-  } else {
-    console.error("Style element not found");
-  }
+    console.log(`Changing style to: ${styleName}`);
+    const styleElement = document.getElementById("dynamic-style");
+    if (styleElement) {
+        styleElement.href = styles[styleName];
+        console.log(`Style changed to: ${styleElement.href}`);
+    }
+    else {
+        console.error("Style element not found");
+    }
 }
 // Attach the function to the global window object
 window.changeStyle = changeStyle;
-/******/ })()
-;
